b3c247fafd9fe7e931e4b785af2eb0be3203f661
= <=>
2024-10-22 08:35:51 +0700

    create aplikasi persegi dan lingkaran

---
+++ lingkaran.py
@@ -0,0 +1,8 @@
+#lingkaran
+r = int(input('masukan jari jari'))
+
+diameter = r * 2
+keliling = 2 * 3.14 * r 
+
+print (f'diameter lingkaran adalah:{diameter} ')
+print (f' keliling lingkaran adalah:{keliling}')
---
+++ persegi.py
@@ -0,0 +1,9 @@
+#persegi 
+ 
+sisi = int (input('masukan sisi:'))
+
+keliling = sisi * sisi 
+luas = 4 * sisi
+
+print (f'keliling dari persegi adalah{keliling}')
+print (f'luas dari persegi adalah{luas}')
