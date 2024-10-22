99278d0121e49e165997a1adceea3c600d0c35df
= <=>
2024-10-22 08:43:11 +0700

    create aplikasi persegi panjang

---
+++ persegi panjang.py
@@ -0,0 +1,10 @@
+#persegi panjang
+
+panjang = int (input('masukan panjang: '))
+lebar = int (input('masukan luas:'))
+
+keliling = 2 * (panjang + lebar)
+luas = panjang * lebar
+
+print (f'keliling persegi panjang adalah:{keliling}')
+print(f'luas persegi panjang adalah:{lebar}')
