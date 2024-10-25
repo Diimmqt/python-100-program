ab71c36dbdfe020d7e94982ca901166871d4a445
= <=>
2024-10-25 08:13:55 +0700

    push program 25/10/24

---
+++ balok.py
@@ -0,0 +1,16 @@
+print ('='*15)
+print ('balok')
+print ('='*15)
+
+pl = int (input('masukan sisi pl:'))
+lt = int (input('masukan sisi lt:'))
+pt = int (input('masukan sisi pt:'))
+p = int (input('masukan panjang:'))
+l = int (input('masukan lebar:'))
+t = int (input('masukan tinggi:'))
+
+luas = 2 (pl + lt + pt)
+volume = p * l * t
+
+print (f'luas ={luas}')
+print (f'volume = {volume}')
---
+++ limas segitiga.py
@@ -0,0 +1,13 @@
+print ('='*15)
+print ('limas segitiga')
+print ('='*15)
+
+la =int (input('masukan luas alas:'))
+ls = int (input('masukan luas sisi:'))
+t = int (input('masukan tinggi:'))
+
+luas = la + ls
+volume = 1/3 * la * t
+
+print (f'luas = {luas}')
+print (f'volume = {volume}')
