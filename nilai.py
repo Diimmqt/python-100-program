ec79de621874a35ca31cb63c6f6d1058475c2f34
= <=>
2024-11-16 12:19:51 +0700

    pushh program

---
+++ nilai.py
@@ -0,0 +1,6 @@
+nilai = int (input('masukan nilai anda: '))
+
+if nilai > 78:
+    print ('lulus')
+elif nilai < 78:
+    print ("gagal")
---
+++ pengulangan.py
@@ -0,0 +1,5 @@
+jumlah = int (input('masukan  berapa list kata: ')) 
+a = str (input ('masukan kata yang ingin kamu tampilkan:'))
+
+for i in range(jumlah):
+    print (f'{i + 1},{a}.')
