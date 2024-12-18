fff9fb0a6488f7264cf277d3343c41aae85c06d7
= <=>
2024-12-18 15:46:08 +0700

    push program

---
+++ membulatkan bilangan.py
@@ -0,0 +1,3 @@
+num=float(input('masukan angka desimal: '))
+
+print (round(num))
---
+++ perbandingan.py
@@ -0,0 +1,7 @@
+a= int (input('masukan angka pertama: '))
+b= int (input('masukan angka kedua:'))
+
+if a > b:
+    print('angka A lebih besar dari pada angka B')
+else:
+    print('angka A lebih kecil daripada angka B')
