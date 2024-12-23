84e8ee1929ae778726f743596bc942475e4d1a71
= <=>
2024-12-24 06:44:05 +0700

    p

--- menghitung satuan m kebawah.py.py
+++ menghitung satuan m kebawah.py.py
@@ -6,15 +6,15 @@ angka= int (input("masukan angka satuan:"))
 
 if operator == 'km':
     res = angka * 1000000
-elif operator == 'cm':
+elif operator == 'hm':
     res = angka * 100000
-elif operator == 'dm':
+elif operator == 'dam':
     res = angka * 10000
 elif operator == 'm':
     res = angka * 1000
-elif operator == 'dam':
+elif operator == 'dm':
     res = angka *100
-elif operator == 'hm':
+elif operator == 'cm':
     res = angka * 10
 
 print (f'konvensi dari {operator} ke mm adalah {res}')
